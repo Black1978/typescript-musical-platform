8ea9288d4676f15b983e4ee890424689c6f98628
Black1978 <chernyi.andrei@gmail.com>
2022-01-04 16:46:40 +0100

    The static files sharing is added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { FileModule } from "./file/file.module";
 import { TrackModule } from "./track/track.module";
+import * as path from 'path'
+import { ServeStaticModule } from "@nestjs/serve-static";
 
 @Module({
     imports: [
         MongooseModule.forRoot('mongodb+srv://root:root12345@cluster0.2bve0.mongodb.net/music-platform?retryWrites=true&w=majority'),
         TrackModule,
-        FileModule
+        FileModule,
+        ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static'),})
     ]
 })
 export class AppModule {
